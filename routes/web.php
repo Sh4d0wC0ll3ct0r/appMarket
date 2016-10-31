9280ee3389d73fe79bc21a09c11d0f02760af2bd
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2016-10-30 22:07:57 -0500

    Comentando algunas rutas que no se usan

--- routes/web.php
+++ routes/web.php
@@ -16,27 +16,27 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware'=>['web']],function(){
-    route::get('usuario',function(){
+  /*  route::get('usuario',function(){
         return 'Hola Mundo';
     });
     //Rutas con parametros y restricciones
     route::get('usuario/{codigo}',function($codigo){
         return 'Hola Usuario '.$codigo;
     })->where('codigo','[0-9]+');
-
-    route::get('animales/{nombre}',function($nombre){
+*/
+    /*route::get('animales/{nombre}',function($nombre){
         return 'Hola '.$nombre;
-    })->where('nombre','[A-Za-z]+');
+    })->where('nombre','[A-Za-z]+');*/
     //Rutas apuntando a un controlador
-    route::get('producto','Producto\ProductoController@index');
+    //route::get('producto','Producto\ProductoController@index');
     //Controlador con recursos REST
-    route::resource('marca','Producto\MarcaController');
+    //route::resource('marca','Producto\MarcaController');
     /****************************************************/
 
 
-    route::get('panel','Desktop\AdministradorController@panel');
-    route::get('access','Desktop\AdministradorController@access');
-    route::get('reports','Desktop\AdministradorController@reports');
+    //route::get('panel','Desktop\AdministradorController@panel');
+    //route::get('access','Desktop\AdministradorController@access');
+    //route::get('reports','Desktop\AdministradorController@reports');
 
 /*****************************************************************************/
 
